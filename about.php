8f0574021665ec1e1b653767967e4eec615a8874
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2019-01-06 19:22:39 -0500

    Update about.php

--- about.php
+++ about.php
@@ -1,7 +1,3 @@
-<html>
-    <head>
-        <title>Mixin Up Spirits</title>
-
 <style type="text/css">
     /*BODY*/
 body {
@@ -9,7 +5,6 @@ body {
     font-family: 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     background-color: white;
 }
-
 /*HEADER*/
 #header {
     width: 100%;
@@ -17,74 +12,79 @@ body {
     background-color: black;
     
 }
-
 h1, #nav {
     display: inline-block;
     color: white;
 }
-
 h1 {
     margin-left: 53px;
 }
-
 h4 {
     display: inline-block;
     margin-left: 33px;
     color: white;
 }
-
 a:link {
     text-decoration: none;
     color: white;
 }
-
 a:hover {
     color: lightgrey;
 }
-
 a:visited {
     color: #ffa208;
 }
-
-
 /*ABOUT PARAGRAPH*/
 p {
-    width: 30%;
+    width: 35%;
     height: auto;
     font-size: 18px;
-    margin-left: 80px;
-    margin-top: 60px;
-
+    margin-left: 10%;
+    margin-top: 5%;
+    text-align: center;
+    z-index: 1;
+    top:50%;
+    color: black;
+    background-color: white;
+    opacity: 0.85;
+    padding: 3%;
+    position: absolute;
 }
-
-img {
-    width: 70%;
-    height: auto;
-    margin-left: 100px;
-    margin-top: 110px;
-    image-resolution: 550px;
+/*SLIDESHOW*/
+.slideshow {
+   width: 75%;
+   margin: auto;
+  display: block;
+  margin-top: 5%;
+  z-index: -1;
+    
 }
-
-p, img {
-    display: inline-block;
-}
-
-
-
 /*FOOTER*/
 hr {
     color: #ffa208;
-
 }
-
 #footer {
     margin-top: 60px;
-    bottom: 2;
-    position: relative;
+    bottom: 1;
+    position: absolute;
     width:60%;
 }
+/*MOBILE*/
+@media only screen and (max-width: 700px) {
+    #header {
+        height: 24%;
+    }
+    .slideshow {
+    display:none;
+}
+    p {
+    width: 70%;
+    font-size: 1em;
+    z-index: -1;
+    top:25%;
+}
+}
 </style>
-</head>
 <!--HEADER-->
 <div id="header">
 <a href="index.php"><h1>Mixin&rsquo; Up Spirits</h1></a>
@@ -94,10 +94,40 @@ hr {
 <a href="contact.php"><h4>Contact</h4></a>
 </div>
 </div>
-<p>Based in Washington, DC, we provide bartending services for a wide range of clientele and venues, even non-alcoholic and kiddie events! We at Mixin&rsquo; Up Spirits pride ourselves on our <b><em>professionalism</em></b>, our <b><em>versatility</em></b>, and <b><em>personability</em></b>. Mixin&rsquo; Up Spirits is guarunteed to serve a good drink for those good times.</p>
-<p><img src="https://res.cloudinary.com/rapha3l/image/upload/v1531424919/event_types_fhmviq.jpg" /></p>
-<div id="footer"><hr />
+
+<div class="slideshow" style="width:75%">
+        <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/good_times_nsozhv.jpg" style="width:100%">
+        <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/logo_photo_sh7fnw.jpg" style="width:100%">
+        <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/good_times_2_hjgpyf.jpg" style="width:100%">
+        <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424919/event_types_fhmviq.jpg" style="width:100%">
+
+        
+    </div> 
+
+    <p>Based in Washington, DC, we provide bartending services for a wide range of clientele and venues, even non-alcoholic and kiddie events! We at Mixin&rsquo; Up Spirits pride ourselves on our <b><em>professionalism</em></b>, our <b><em>versatility</em></b>, and <b><em>personability</em></b>. Mixin&rsquo; Up Spirits is guarunteed to serve a good drink for those good times.</p>
+
+<!--<div id="footer"><hr />
 <h6>Mixin&rsquo; Up Spirits, LLC &copy; est. 2017</h6>
 </div>
-<body>
-<html>
+-->
+
+
+<script>
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+            var i;
+            var x = document.getElementsByClassName("mySlides");
+            for (i = 0; i < x.length; i++) {
+               x[i].style.display = "none";  
+            }
+            myIndex++;
+            if (myIndex > x.length) {myIndex = 1}    
+            x[myIndex-1].style.display = "block";  
+            setTimeout(carousel, 2000); // Change image every 2 seconds
+        }
+        </script>
+    
+
+</body>
