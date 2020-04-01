d0325171bb6107a6b6fb1519730bb733026803b2
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2020-04-01 17:44:16 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <meta property="og:title" content="Mixin' Up Spirits">
+<meta property="og:description" content="Mobile Mixologists and Bartenders in the Washington, DC Metro Area">
+<meta property="og:image" content="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/logo_photo_sh7fnw.jpg">
+<meta property="og:url" content="www.mixinupspirits.com">
+    <meta name="twitter:image" content="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/logo_photo_sh7fnw.jpg">
+    
+
+<style>
+#header {
+    width: 100%;
+    height: 18%;
+    background-color: black;
+}
+h1, #nav {
+    display: inline-block;
+    color: whitesmoke;
+}
+h1 {
+    margin-left: 53px;
+    
+}
+h4 {
+    display: inline-block;
+    margin-left: 33px;
+    color: #ffa208;
+    font-size:2em;
+}
+
+@media only screen and (max-width: 700px) {
+
+    #header {
+        height: 26%;
+    }
+
+    h4 {
+    display: inline-block;
+    margin-left:33px;
+    color: #ffa208;
+}
+
+}
+</style>
+</head>
+<!--HEADER-->
+    <div id="header">
+            <a href="index.php"><h1>Mixin' Up Spirits</h1></a>
+       <div id="nav">
+          <a href="about.php"><h4>About</h4></a>
+          <a href="gallery.php"><h4>Gallery</h4></a>
+          <a href="contact.php"><h4>Contact</h4></a>
+       </div>
+        </div>
+
+
+</html>
