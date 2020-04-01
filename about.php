2e2558767360c010ec3132ab20d0e71fe5882756
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2020-04-01 17:43:35 -0400

    Update about.php

--- about.php
+++ about.php
@@ -1,120 +1,137 @@
-<style type="text/css">
-    /*BODY*/
-body {
-    margin:0;
-    font-family: 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    background-color: white;
-}
-/*HEADER*/
-#header {
-    width: 100%;
-    height: 18%;
-    background-color: black;
-    
-}
-h1, #nav {
-    display: inline-block;
-    color: white;
-}
-h1 {
-    margin-left: 53px;
-}
-h4 {
-    display: inline-block;
-    margin-left: 33px;
-    color: white;
-}
-a:link {
-    text-decoration: none;
-    color: white;
-}
-a:hover {
-    color: lightgrey;
-}
-a:visited {
-    color: #ffa208;
-}
-/*ABOUT PARAGRAPH*/
-p {
-    width: 35%;
-    height: auto;
-    font-size: 18px;
-    margin-left: 10%;
-    margin-top: 5%;
-    text-align: center;
-    z-index: 1;
-    top:50%;
-    color: black;
-    background-color: white;
-    opacity: 0.85;
-    padding: 3%;
-    position: absolute;
-}
-/*SLIDESHOW*/
-.slideshow {
-   width: 75%;
-   margin: auto;
-  display: block;
-  margin-top: 5%;
-  z-index: -1;
-    
-}
-/*FOOTER*/
-hr {
-    color: #ffa208;
-}
-#footer {
-    margin-top: 60px;
-    bottom: 1;
-    position: absolute;
-    width:60%;
-}
-/*MOBILE*/
-@media only screen and (max-width: 700px) {
-    #header {
-        height: 24%;
-    }
-    .slideshow {
-    display:none;
-}
-    p {
-    width: 70%;
-    font-size: 1em;
-    z-index: -1;
-    top:25%;
-}
-}
-</style>
-<!--HEADER-->
-<div id="header">
-<a href="index.php"><h1>Mixin&rsquo; Up Spirits</h1></a>
-<div id="nav">
-<a href="about.php"><h4>About</h4></a>
-<a href="gallery.php"><h4>Gallery</h4></a>
-<a href="contact.php"><h4>Contact</h4></a>
-</div>
-</div>
+<html>
+    <head>
+    <title>About | Mixin' Up Spirits</title>
+    <link rel="shortcut icon" href="https://res.cloudinary.com/rapha3l/image/upload/v1531505861/tab_mus_qxsjtj.png">
+            <style type="text/css">
+                /*BODY*/
+            body {
+                margin:0;
+                font-family: 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+             /*   background-color: #FFF5EE; */
+            }
+            /*SLIDESHOW*/
+            .slideshow {
+            width: 75%;
+            margin: auto;
+            display: block;
+            margin-top: 5%;
+            z-index: -1;
+            opacity: 0.8;
+               margin-bottom: 5%; 
+               -webkit-box-shadow: 5px 8px 6px -6px #ffa208;
+	   -moz-box-shadow:5px 8px 6px -6px #ffa208;
+	        box-shadow: 5px 8px 6px -6px #ffa208; 
+            }
 
-<div class="slideshow" style="width:75%">
+            h2 {
+                color:black ;
+                text-shadow: 2px 1px #ffa208;
+                font-size: 56px;
+                letter-spacing: 4px;
+                text-align: center;
+
+            }
+
+            p {
+                width: 55%;
+                font-size: 1em;
+                font-weight:lighter;
+                margin-left: 20%;
+                margin-top: 2%;
+                text-align: center;
+                letter-spacing: 1.5px;
+            }
+
+        .h3 {
+            text-align: center;
+            font-size: 1.5em;
+            background-color: #ffa208;
+            opacity: 0.6;
+            padding: 2.5%;
+            color:white;
+            font-style: italic;
+            letter-spacing: 3px;
+
+            -webkit-box-shadow: 5px 8px 6px -6px black;
+	   -moz-box-shadow:5px 8px 6px -6px black;
+	        box-shadow: 5px 8px 6px -6px black;
+            
+        }
+        
+
+        #info {
+            width: 30%;
+            height: auto;
+            margin:auto;
+            display: block;
+            margin-bottom: 5%; 
+               -webkit-box-shadow: 5px 8px 6px -6px #ffa208;
+	   -moz-box-shadow:5px 8px 6px -6px #ffa208;
+	        box-shadow: 5px 8px 6px -6px #ffa208;
+        }
+            /*FOOTER*/
+            hr {
+                color: #ffa208;
+            }
+            #footer {
+                margin-top: 60px;
+                bottom: 1;
+                position: absolute;
+                width:60%;
+            }
+            /*MOBILE*/
+            @media only screen and (max-width: 700px) {
+                #header {
+                    height: 24%;
+                }
+                .slideshow {
+                display:none;
+            }
+                p {
+                width: 70%;
+                font-size: 1em;
+                z-index: -1;
+                top:25%;
+            }
+            }
+            </style>
+    </head>
+<body>
+<?php require "header.php"; ?>
+
+<h2>What is Mixin' Up Spirits?</h2>
+
+<p>Based in Washington, DC, we provide bartending services for a wide range of clientele and venues, even non-alcoholic and kiddie events! We at Mixin&rsquo; Up Spirits pride ourselves on our <b><em>professionalism</em></b>, our <b><em>versatility</em></b>, and <b><em>personability</em></b>. Mixin&rsquo; Up Spirits is guarunteed to serve a good drink for those good times.</p>
+
+
+<div class="slideshow" style="width:65%">
         <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/good_times_nsozhv.jpg" style="width:100%">
         <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/logo_photo_sh7fnw.jpg" style="width:100%">
         <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424918/good_times_2_hjgpyf.jpg" style="width:100%">
         <img class="mySlides" src="https://res.cloudinary.com/rapha3l/image/upload/v1531424919/event_types_fhmviq.jpg" style="width:100%">
 
-        
-    </div> 
+    </div>
+    <br/>
+  
+    <p class="h3">TIPS & TAM Certified</p>
+<p >You can book us for your next event the confidence that you will not only being paying for good drinks and stellar customer service, but also a safe and responsible atmosphere for everyone. Mixin' Up Spirits staff behind the bar are trained and certified in <b>TIPS (Training for Intervention ProcedureS)</b> and <b>TAM (Techniques of Alcohol Management)</b> to also ensure a safe drinking experience for all of your guests. </p>
+<br/>
+<br/>
 
-    <p>Based in Washington, DC, we provide bartending services for a wide range of clientele and venues, even non-alcoholic and kiddie events! We at Mixin&rsquo; Up Spirits pride ourselves on our <b><em>professionalism</em></b>, our <b><em>versatility</em></b>, and <b><em>personability</em></b>. Mixin&rsquo; Up Spirits is guarunteed to serve a good drink for those good times.</p>
 
-<!--<div id="footer"><hr />
-<h6>Mixin&rsquo; Up Spirits, LLC &copy; est. 2017</h6>
-</div>
--->
+<p class="h3">Bartending  vs  Mixolgy</p>
+<p >What's is the difference?</p>
+<br/>
+<br/>
 
+    <img id="info" src="https://res.cloudinary.com/rapha3l/image/upload/v1554493702/bt_vs_mx_wef98p.png">
+
+   <!--FOOTER-->
+   <?php require "footer.php"; ?>
 
 <script>
+//slideshow
         var myIndex = 0;
-        carousel();
         
         function carousel() {
             var i;
@@ -125,9 +142,14 @@ hr {
             myIndex++;
             if (myIndex > x.length) {myIndex = 1}    
             x[myIndex-1].style.display = "block";  
-            setTimeout(carousel, 2000); // Change image every 2 seconds
+            setTimeout(carousel, 2550); // Change image every 2 seconds
         }
-        </script>
+        carousel();
+
+</script>
+
+
     
 
 </body>
+</html>
