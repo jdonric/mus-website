222206c8b1a63bbfead94fff2826f821c2908d2d
Jiell Richardson <32615427+jdonric@users.noreply.github.com>
2020-04-22 15:09:30 -0400

    Update about.php

--- about.php
+++ about.php
@@ -138,7 +138,7 @@
 
 
 <p class="h3">Bartending  vs  Mixolgy</p>
-<p >What's is the difference?</p>
+	<p >What's the difference between <em>bartending</em> and <em>mixology</em>?</p>
 <br/>
 <br/>
 
